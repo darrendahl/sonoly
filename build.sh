b3ddc0a704ce7107a047961a1c6f85277d059bd7
Darren Dahl <darrencdahl@gmail.com>
2020-03-30 10:42:49 -0300

    fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 pip install -r backend/requirements.txt
+cd frontend/web 
+npm install
+npm run build
+cd ../..
 python backend/manage.py migrate
 python backend/manage.py collectstatic
